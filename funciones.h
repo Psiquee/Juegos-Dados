3485d1f1cba72a9267eee89a369c424215ebe81b
Delesito <deledicquejoaquin@gmail.com>
2024-06-23 08:50:54 -0300

    con puntaje max 2J arreglado y modo revision

--- funciones.h
+++ funciones.h
@@ -3,8 +3,8 @@
 void menu();
 int jugarRonda();
 int jugarModoUnJugador(char nombre[], int puntajeMaxAnterior, char nombreMaxAnterior[]);
-void jugarModoDosJugadores(char nombre1[], char nombre2[], int puntajeMaxAnterior, char nombreMaxAnterior[]);
-void mostrarPuntajeMaximo(int puntajeMax, char nombreMax[]);
+int jugarModoDosJugadores(char nombre1[], char nombre2[], char nombreMax2J ,int puntajeMaxAnterior, char nombreMaxAnterior[]);
+void mostrarPuntajeMaximo(int puntajeMax, char nombreMax[],int puntajeMax2J, char nomMax2J);
 void copiarVector(char v[], char v2[], int tam);
 
 
